9768b8df0e4c38736e2eaf62aa790a02f1fd4050
Michael Dougherty <maackle.d@gmail.com>
2014-02-17 02:16:29 -0800

    location-specific order page

--- app/coffee/main.coffee
+++ app/coffee/main.coffee
@@ -7,3 +7,23 @@ $ ->
 			speed = $(el).attr('data-bg-speed')
 			console.log "-#{y/speed}px"
 			$(this).scrollTop("-#{y/speed}px")
+
+	$orderChoices = $('.order-page .location-choices')
+	$orderItems = $('.order-page .squareup-embeds')
+
+	resetOrderPage = (e) ->
+		$orderChoices.fadeIn()
+		$orderItems.hide()
+		$orderItems.find('.local-items, .anywhere-items').hide()
+
+	unfoldOrderPage = (e) ->
+		$orderChoices.hide()
+		$orderItems.fadeIn('slow')
+		if $(this).hasClass('local')
+			$orderItems.find('.local-items').show()
+		else
+			$orderItems.find('.anywhere-items').show()
+	
+	$orderChoices.find('.choice').click unfoldOrderPage
+	$orderItems.find('a.reset').click resetOrderPage
+	resetOrderPage()
--- www/assets/scripts/main.js
+++ www/assets/scripts/main.js
@@ -1,6 +1,7 @@
 (function() {
   $(function() {
-    return $(window).scroll(function(e) {
+    var $orderChoices, $orderItems, resetOrderPage, unfoldOrderPage;
+    $(window).scroll(function(e) {
       var $scrollables, y;
       $scrollables = $('[data-bg-speed]');
       y = $(this).scrollTop();
@@ -11,6 +12,25 @@
         return $(this).scrollTop("-" + (y / speed) + "px");
       });
     });
+    $orderChoices = $('.order-page .location-choices');
+    $orderItems = $('.order-page .squareup-embeds');
+    resetOrderPage = function(e) {
+      $orderChoices.fadeIn();
+      $orderItems.hide();
+      return $orderItems.find('.local-items, .anywhere-items').hide();
+    };
+    unfoldOrderPage = function(e) {
+      $orderChoices.hide();
+      $orderItems.fadeIn('slow');
+      if ($(this).hasClass('local')) {
+        return $orderItems.find('.local-items').show();
+      } else {
+        return $orderItems.find('.anywhere-items').show();
+      }
+    };
+    $orderChoices.find('.choice').click(unfoldOrderPage);
+    $orderItems.find('a.reset').click(resetOrderPage);
+    return resetOrderPage();
   });
 
 }).call(this);
